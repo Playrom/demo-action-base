f08563495601b5783546b9e836e93126b1f53aff
Giorgio Romano <lavoro@giorgioromano.it>
2024-05-29 14:24:01 +0200

    Add demo

---
+++ DemoBase/DemoBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoBaseApp.swift
+//  DemoBase
+//
+//  Created by Giorgio Romano on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
